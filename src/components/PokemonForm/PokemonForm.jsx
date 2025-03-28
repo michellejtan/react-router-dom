423b6a9769e234c3343f4f35e72040c164fc32ea
michellejtan <michelle0223@gmail.com>
2025-03-28 13:36:03 -0700

    wip: build initial new page page component

---
+++ src/components/PokemonForm/PokemonForm.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+
+const initialState = {
+    name: '',
+    weight: 0,
+    height: 0,
+};
+
+const PokemonForm = (props) => {
+    const [formData, setFormData] = useState(initialState);
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        props.addPokemon(formData);
+        setFormData(initialState);
+    };
+
+    // const handleChange = ({ target }) => {
+    //     setFormData({
+    //         ...formData,
+    //         [target.name]: target.value
+    //     });
+    // };
+    const handleChange = (evt) => {
+        setFormData({
+            ...formData, //replace existing state, if remove, will change most recent NOT all
+            [evt.target.name]: evt.target.value
+        });
+    };
+    return (
+        <main>
+            <h2>New Pokemon</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Name:</label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                />
+                <label htmlFor="weight">Weight:</label>
+                <input
+                    type="number"
+                    id="weight"
+                    name="weight"
+                    value={formData.weight}
+                    onChange={handleChange}
+                />
+                <label htmlFor="height">Height:</label>
+                <input
+                    type="number"
+                    id="height"
+                    name="height"
+                    value={formData.height}
+                    onChange={handleChange}
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </main>
+    );
+};
+
+export default PokemonForm;
