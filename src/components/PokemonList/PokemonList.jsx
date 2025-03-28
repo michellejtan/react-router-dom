eb06324faa567626bb46d907e5ed8b0bdb920c55
michellejtan <michelle0223@gmail.com>
2025-03-28 13:36:37 -0700

    wip: added dynamic link to list page component

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,14 +1,20 @@
+import { Link } from "react-router";
+
 const PokemonList = (props) => {
-    return (
-      <>
-        <h2>Pokemon</h2>
-        <ul>
-          {props.pokemon.map((currentPokemon) => (
-            <li key={currentPokemon.name}>{currentPokemon.name}</li>
-          ))}
-        </ul>
-      </>
-    );
-  };
-  
-  export default PokemonList;  
+  return (
+    <>
+      <h2>Pokemon</h2>
+      <ul>
+        {props.pokemon.map((currentPokemon) => ( //id better than name
+          <li key={currentPokemon._id}> 
+          <Link to={`/pokemon/${currentPokemon._id}`}>
+            {currentPokemon.name}
+          </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default PokemonList;  
